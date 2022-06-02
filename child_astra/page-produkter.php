24531ad497b24771deb974baa9b49c83e9093d56
Nanna Hansen <nann257j@stud.kea.dk>
2022-06-02 20:17:13 +0200

    first commit

---
+++ child_astra/page-produkter.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+<template>
+  <article id="produkt-article">
+    <img class="billede1" src="" alt="" />
+    <div class="produkt-info">
+      <h2></h2>
+      <p class="pris"></p>
+    </div>
+  </article>
+</template>
+
+<div id="content" class="site-content clr">
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main">
+      <!-- splashbillede -->
+     
+      <!-- produkter og knapper -->
+
+      <nav id="filtrering">
+        <button data-produkt="alle">Alle produkter</button>
+      </nav>
+
+      <h1 class="produkttitel">Produkter</h1>
+
+      <section class="produktcontainer"></section>
+
+      <!-- poter -->
+      
+      <!-- footer -->
+      
+      <!-- footer slut -->
+    </main>
+    <!-- #main -->
+    <script>
+      let produkter;
+      let categories;
+      let filterProdukt = "alle";
+      let h1 = document.querySelector(".produkttitel");
+
+      // link til wp database alle produkter
+      const dbUrl =
+        "https://nannatorp.dk/kea/10_eksamensprojekt/woofydays-wp/wp-json/wp/v2/produkt?per_page=100";
+      // link til wp database alle kategorier
+      const catUrl =
+        "https://nannatorp.dk/kea/10_eksamensprojekt/woofydays-wp/wp-json/wp/v2/categories";
+
+      // Henter data ind
+      async function hentData() {
+        const data = await fetch(dbUrl);
+        const catdata = await fetch(catUrl);
+        //fylder data i variablerne
+        produkter = await data.json();
+        categories = await catdata.json();
+        console.log(produkter);
+        visProdukter();
+        opretKnapper();
+      }
+
+      // function der opretter knapper med kategori id som data attribut
+      function opretKnapper() {
+        categories.forEach((cat) => {
+          document.querySelector(
+            "#filtrering"
+          ).innerHTML += `<button class="filter" data-produkt="${cat.id}">${cat.name}</button>`;
+        });
+        addEventListenersToButton();
+      }
+
+      // funcktion der laver en click eventListener til knapperne
+      // kigger på alle de knapper som er sat ind i nav
+
+      function addEventListenersToButton() {
+        document.querySelectorAll("#filtrering button").forEach((elm) => {
+          elm.addEventListener("click", filtrering);
+        });
+      }
+
+      // function der filtrere kanpperne når der klikkes (this = det der er klikket på)
+      function filtrering() {
+        filterProdukt = this.dataset.produkt;
+        console.log(filterProdukt);
+
+        //skrifter overskriften
+        h1.textContent = this.textContent;
+
+        visProdukter();
+      }
+
+      function visProdukter() {
+        console.log(visProdukter);
+        let temp = document.querySelector("template");
+        let container = document.querySelector(".produktcontainer");
+
+        //ryd visings container inden ny loop/visning
+        container.innerHTML = "";
+
+        produkter.forEach((produkt) => {
+          //viser produkterne afhæning af hvilken knap der er trykket på
+          // parseInt gør det bliver lavet om til et tal
+          if (
+            filterProdukt == "alle" ||
+            produkt.categories.includes(parseInt(filterProdukt))
+          ) {
+            //klon template og indsæt data fra JSON
+            let klon = temp.cloneNode(true).content;
+            klon.querySelector("h2").textContent = produkt.title.rendered;
+            klon.querySelector(".pris").textContent = produkt.pris + " kr.";
+            klon.querySelector("img").src = produkt.billede[0].guid;
+
+            // gør man kan klikke på et produkt og den åbner singleview.
+            // produkt.link tager fat i linket til singleview siden
+            klon.querySelector("article").addEventListener("click", () => {
+              location.href = produkt.link;
+            });
+
+            //viser det i dommen
+            container.appendChild(klon);
+          }
+        });
+      }
+      hentData();
+    </script>
+  </div>
+  <!-- #primary -->
+</div>
+<!-- #content -->
+
+<?php get_footer(); ?>
---
+++ child_astra/single-produkt.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main">
+    <article id="singleview">
+      <div class="billedecontainer"></div>
+      <h2></h2>
+      <p class="pris"></p>
+    </article>
+    <article id="singleview2">
+      <p class="produktbeskrivelse">Produktbeskrivelse</p>
+      <p class="underoverskrift-1"></p>
+      <p class="beskrivelse-1"></p>
+      <p class="underoverskrift-2"></p>
+      <p class="beskrivelse-2"></p>
+      <p class="underoverskrift-3"></p>
+      <p class="beskrivelse-3"></p>
+      <p class="underoverskrift-4"></p>
+      <p class="beskrivelse-4"></p>
+      <p class="underoverskrift-5"></p>
+      <p class="beskrivelse-5"></p>
+      <p class="underoverskrift-6"></p>
+      <p class="beskrivelse-6"></p>
+    </article>
+     
+    <!-- footer -->
+    
+      <!-- footer slut -->
+  </main>
+  <script>
+
+      let produkt;
+
+      //link til database med en php snippet med en function der gør vi kan få postens id.
+      const url = "https://nannatorp.dk/kea/10_eksamensprojekt/woofydays-wp/wp-json/wp/v2/produkt/"+<?php echo get_the_ID() ?>;
+
+
+      async function hentData() {
+     console.log("hentData");
+
+     const data = await fetch(url);
+     produkt = await data.json();
+     visProdukter();
+      }
+      function visProdukter() {
+       console.log(produkt.billede.guid);
+
+     document.querySelector("h2").textContent = produkt.title.rendered;
+     document.querySelector(".pris").textContent = produkt.pris + " kr.";
+    document.querySelector(".underoverskrift-1").textContent =
+             produkt.underoverskriftet;
+    document.querySelector(".underoverskrift-2").textContent =
+             produkt.underoverskriftto;
+    document.querySelector(".underoverskrift-3").textContent =
+             produkt.underoverskrifttre;
+    document.querySelector(".underoverskrift-4").textContent =
+             produkt.underoverskriftfire;
+    document.querySelector(".underoverskrift-5").textContent =
+             produkt.underoverskriftfem;
+    document.querySelector(".underoverskrift-6").textContent =
+             produkt.underoverskriftseks;
+    document.querySelector(".beskrivelse-1").textContent =
+             produkt.beskrivelseet;
+    document.querySelector(".beskrivelse-2").textContent =
+             produkt.beskrivelseto;
+    document.querySelector(".beskrivelse-3").textContent =
+             produkt.beskrivelsetre;
+    document.querySelector(".beskrivelse-4").textContent =
+             produkt.beskrivelsefire;
+    document.querySelector(".beskrivelse-5").textContent =
+             produkt.beskrivelsefem;
+    document.querySelector(".beskrivelse-6").textContent =
+             produkt.beskrivelseseks;
+
+
+    let first_iteration = true;
+
+           //et array af billder og det looper vi igennem  med en html streng vi kloner billede containeren
+           //og sætter en inner html på med img så den løber igennem billederne til der ikke er flere med forEach
+           //hvis false tilføj classen billedeStor, hvis true tilføj ikke.
+           produkt.billede.forEach((pic) => {
+            let img;
+             if (first_iteration) {
+                img = `<img class="billedeStor" src="${pic.guid}" alt="" />`;
+                first_iteration = false;
+              }else{
+                img = `<img src="${pic.guid}" alt="" />`;
+               }
+
+             document.querySelector(".billedecontainer").innerHTML += img;
+           });
+      }
+      hentData();
+  </script>
+</div>
+<!-- #primary -->
+
+<?php get_footer(); ?>
